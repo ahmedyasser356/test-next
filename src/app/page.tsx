c0f59e8571d4c2b70062e8e37f0716900dab0d82
ahmedyasser365 <ahmedyasser573he7@gmail.com>
2025-06-25 23:20:20 +0300

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+
+  let [ahmed , setahmed] = useState('welcome to the next app ')
   return (
     <div className={styles.page}>
       <main className={styles.main}>
        
     
 
-           
+           <h1>{ahmed}</h1>
          
        <h1>Ahmed Yasser</h1>
       </main>
