3cd7487b551a474dcd927786cefaec882d7a3bae
ahmedyasser365 <ahmedyasser573he7@gmail.com>
2025-06-25 23:28:33 +0300

    v3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,21 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  let {push}=useRouter()
 
+  function goToLogin(){
+push('/login')
+  }
   let [ahmed , setahmed] = useState('welcome to the next app ')
   return (
     <div className={styles.page}>
       <main className={styles.main}>
        
     
-
+           <h1 onClick={goToLogin}>Click Here</h1>
            <h1>{ahmed}</h1>
          
        <h1>Ahmed Yasser</h1>
